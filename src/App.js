517b3c5355b99a64ce473cc77089788478f9a937
Yusuf <alpyusuf007@gmail.com>
2020-07-27 08:19:04 -0700

    User entry is updated in the fetch URL

--- src/App.js
+++ src/App.js
@@ -11,26 +11,31 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    // const url = 'https://www.googleapis.com/books/v1/volumes?q=';
-    fetch(this.state.url)
-      .then(res => {
-        if(!res.ok) {
-          throw new Error('Something went wrong!');
-        }
-        return res;
-      })
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          books: data.items,
-          error: null
-        });
-      })
-      .catch(err => {
-        this.setState({
-          error: err.message
-        });
+    this.fetchData(this.state.url)
+  }
+  componentDidUpdate(){
+    this.fetchData(this.state.url)
+  }
+  fetchData(url){
+    fetch(url)
+    .then(res => {
+      if(!res.ok) {
+        throw new Error('Something went wrong!');
+      }
+      return res;
+    })
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        books: data.items,
+        error: null
       });
+    })
+    .catch(err => {
+      this.setState({
+        error: err.message
+      });
+    });
   }
   handleAdd(url){
     this.setState({
